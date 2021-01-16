bb551709ccd84eebd9ca6b9cd523a82b0f30ad9e
Himanshu Gupta <thinktank.himanshu@gmail.com>
2021-01-16 09:52:43 +0530

    completed all 15

---
+++ 15-cocktails/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae sit
+        delectus illo nemo molestias similique dolor voluptatum dolore aliquid
+        totam vitae animi voluptas, ratione voluptatem praesentium veritatis
+        repellendus culpa aperiam incidunt harum sunt necessitatibus.
+        Repellendus hic impedit nesciunt veniam modi.
+      </p>
+    </section>
+  );
+};
+
+export default About;
